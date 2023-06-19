62548b137c2c1d108ca67358b9854b54ad623260
susilo001 <susilo.2000@students.amikom.ac.id>
2023-06-19 18:21:13 +0700

    fix accessbility in auth pages

--- resources/js/Pages/Auth/VerifyEmail.jsx
+++ resources/js/Pages/Auth/VerifyEmail.jsx
@@ -18,7 +18,7 @@ export default function VerifyEmail({ status }) {
       <div className="mb-4 text-sm">
         Thanks for signing up! Before getting started, could you verify your
         email address by clicking on the link we just emailed to you? If you
-        didn't receive the email, we will gladly send you another.
+        didn&apos;t receive the email, we will gladly send you another.
       </div>
 
       {status === "verification-link-sent" && (
